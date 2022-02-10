23acdd0559e8dc445823b8d15dbb35d99cdb7b83
byObin <byobin26@gmail.com>
2022-02-10 23:58:58 +0900

    두 수 비교하기

---
+++ 1330/1330/source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	int b;
+	scanf_s("%d %d", &a, &b);
+	if (a > b) {
+		printf(">");
+	}
+	else if(a < b) {
+		printf("<");
+	}
+	else if (a == b) {
+		printf("==");
+	}
+}
