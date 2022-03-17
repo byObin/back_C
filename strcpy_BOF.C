6d8e28d43bbce8fa8c12f1906ea685672003d096
byObin <byobin26@gmail.com>
2022-03-17 23:07:41 +0900

    LOB level1

---
+++ strcpy_BOF.C
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+    char buffer[256];
+    if(argc < 2){
+        printf("argv error\n");
+        exit(0);
+    }
+    strcpy(buffer, argv[1]);
+    printf("%s\n", buffer);
+}
